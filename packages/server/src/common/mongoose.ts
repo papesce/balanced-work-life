446cb61382c920a1b687ca5138047e5577f4e982
Pablo Pesce <papesce@gmail.com>
2020-01-27 22:02:37 -0300

    setup remote .env

--- packages/server/src/common/mongoose.ts
+++ packages/server/src/common/mongoose.ts
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
 
 export default class Mongoose {
-  connectionURI = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
-
+  connectionURI = `${process.env.MONGO_URI}`;
   constructor() {
   }
 
   init() {
     //Set up default mongoose connection
-    mongoose.connect(this.connectionURI);
+    mongoose.connect(this.connectionURI,  {useNewUrlParser: true, useUnifiedTopology: true} );
 
     // Get Mongoose to use the global promise library
     mongoose.Promise = global.Promise;
