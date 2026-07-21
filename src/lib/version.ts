29d237ed628ed77c981091717e77e2f51f6ec1fe
Pablo Pesce <papesce@gmail.com>
2026-07-21 14:42:43 -0300

    fix: use NEXT_PUBLIC_APP_VERSION env var for UI version display

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,1 +1,3 @@
-export { version as APP_VERSION } from "../../package.json";
+import { version as pkgVersion } from "../../package.json";
+
+export const APP_VERSION: string = process.env.NEXT_PUBLIC_APP_VERSION || pkgVersion;
